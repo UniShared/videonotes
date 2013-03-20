6bc0fa796df3fcc9233816f04940b0f62890e84f
Arnaud BRETON <arnaud@unishared.com>
2013-03-20 10:56:34 +0100

    String.format prototype

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,19 @@ var EditorState = {
     READONLY: 4
 };
 
+//first, checks if it isn't implemented yet
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    };
+}
+
 google.load('picker', '1');
 //gapi.load('drive-share');
 
