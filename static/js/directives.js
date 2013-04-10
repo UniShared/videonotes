a0413005a652382b0d118d63e44f73d4d5c0a765
Arnaud BRETON <arnaud@unishared.com>
2013-04-10 09:38:30 -0700

    Full directive spec

--- static/js/directives.js
+++ static/js/directives.js
@@ -3,46 +3,46 @@
 var module = angular.module('app.directives', []);
 
 module.directive('aceEditor',
-    function (editor) {
+    ['editor', function (editor) {
         return {
-            restrict:'A',
+            restrict: 'A',
             scope: {
                 sync: "="
             },
-            link:function (scope, element) {
+            link: function (scope, element) {
                 editor.rebind(element[0]);
 
-                scope.$watch('sync', function(newValue, oldValue) {
-                    if(newValue !== oldValue) {
-                        newValue ? $('.ace_gutter').removeClass('inactive') : $('.ace_gutter').addClass('inactive');
+                scope.$watch('sync', function (newValue, oldValue) {
+                    if (newValue !== oldValue) {
+                        newValue ? element.find('.ace_gutter').removeClass('inactive') : element.find('.ace_gutter').addClass('inactive');
                     }
                 }, true);
             }
         };
-    });
+    }]);
 
 module.directive('html5VideoPlayer',
-    function (video) {
+    ['video', function (video) {
         return {
-            restrict:'A',
-            link:function (scope, element) {
+            restrict: 'A',
+            link: function (scope, element) {
                 video.bindVideoPlayer(element[0]);
             }
         };
-    });
+    }]);
 
 module.directive('star',
     function () {
         return {
-            restrict:'E',
-            replace:true,
-            scope:{
-                val:'=value',
+            restrict: 'E',
+            replace: true,
+            scope: {
+                val: '=',
                 // Value bound to
-                eventFn:'&click'
+                eventFn: '&click'
                 // Optional expression evaluated on click
             },
-            link:function (scope, element) {
+            link: function (scope, element) {
                 element.bind('click',
                     function () {
                         scope.$apply(function () {
@@ -51,16 +51,16 @@ module.directive('star',
                         scope.$eval(scope.eventFn, scope.val);
                     });
             },
-            template:'<i class="star" ng-class="{\'icon-star\' : val, \'icon-star-empty\' : !val}" ng-click="toggle()"></i>'
+            template: '<i class="star" ng-class="{\'icon-star\' : val, \'icon-star-empty\' : !val}" ng-click="toggle()"></i>'
         }
     });
 
 module.directive('alert',
-    function ($rootScope, analytics) {
+    ['$rootScope', 'analytics', function ($rootScope, analytics) {
         return {
-            restrict:'E',
-            replace:true,
-            link:function (scope, element) {
+            restrict: 'E',
+            replace: true,
+            link: function (scope, element) {
                 $rootScope.$on('error',
                     function (event, data) {
                         scope.$apply(function () {
@@ -73,57 +73,33 @@ module.directive('alert',
                     $(element).hide();
                 };
             },
-            template:'<div class="hide alert alert-error">' +
+            template: '<div class="hide alert alert-error">' +
                 '  <span class="close" ng-click="close()">&times;</span>' +
                 '  {{message}}' +
                 '</div>'
         }
-    });
+    }]);
 
-module.directive('playPauseVideoHtml5', function(video) {
+module.directive('bootstrapTooltip', function () {
     return {
-        link: function(scope, element, attrs) {
-            scope.$watch(attrs.playPauseVideoHtml5, function(newValue, oldValue) {
-                if(newValue != oldValue && video.player) {
-                    newValue ? video.player.play() : video.player.pause();
-                }
-            });
-        }
-    }
-});
-
-module.directive('playPauseVideoYoutube', function(youtubePlayerApi) {
-    return {
-        link: function(scope, element, attrs) {
-            scope.$watch(attrs.playPauseVideoYoutube, function(newValue, oldValue) {
-                if(newValue != oldValue && youtubePlayerApi.player) {
-                    newValue ? youtubePlayerApi.player.playVideo() : youtubePlayerApi.player.pauseVideo();
-                }
-            });
-        }
-    }
-});
-
-module.directive('bootstrapTooltip', function(youtubePlayerApi) {
-    return {
-        restrict:'A',
-        link: function(scope, element) {
+        restrict: 'A',
+        link: function (scope, element) {
             $(element).tooltip();
         }
     }
 });
 
-module.directive('bootstrapSwitch', function($rootScope) {
+module.directive('bootstrapSwitch', ['$rootScope', function ($rootScope) {
     return {
-        restrict:'E',
+        restrict: 'E',
         scope: {
             property: "="
         },
-        link: function(scope, element) {
+        link: function (scope, element) {
             $(element).bootstrapSwitch();
 
             scope.$watch('property', function () {
-                if(scope.property !== undefined)
+                if (scope.property !== undefined)
                     $(element).bootstrapSwitch('setState', scope.property);
             });
 
@@ -136,4 +112,4 @@ module.directive('bootstrapSwitch', function($rootScope) {
         template: '<div class="switch switch-small">\n    <input type="checkbox" />\n</div>'
 
     }
-});
+}]);
--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -2,18 +2,133 @@
 
 /* jasmine specs for directives go here */
 
-describe('directives', function() {
-  beforeEach(module('myApp.directives'));
+describe('directives', function () {
+    var elm, scope;
+    beforeEach(angular.mock.module('app.directives', 'app.services', 'youtube', 'analytics'));
 
-  describe('app-version', function() {
-    it('should print current version', function() {
-      module(function($provide) {
-        $provide.value('version', 'TEST_VER');
-      });
-      inject(function($compile, $rootScope) {
-        var element = $compile('<span app-version></span>')($rootScope);
-        expect(element.text()).toEqual('TEST_VER');
-      });
+    describe('aceEditor', function () {
+        beforeEach(angular.mock.inject(function ($rootScope, $compile) {
+            elm = angular.element('<div id="editor" ace-editor sync="sync"></div>');
+
+            scope = $rootScope;
+            scope.sync = null;
+            $compile(elm)(scope);
+
+            scope.$digest();
+        }));
+
+        it('Enabling sync should enable ace gutter', angular.mock.inject(function ($compile, $rootScope, editor) {
+            var aceGutter = elm.find('.ace_gutter');
+
+            scope.sync = true;
+            scope.$apply();
+
+            expect(aceGutter).not.toHaveClass('inactive');
+        }));
+
+        it('Disabling sync should disable ace gutter', angular.mock.inject(function ($compile, $rootScope, editor) {
+            var aceGutter = elm.find('.ace_gutter');
+
+            scope.sync = false;
+            scope.$apply();
+
+            expect(aceGutter).toHaveClass('inactive');
+        }));
+
+        it('Toggling sync should toggle ace gutter activation', angular.mock.inject(function ($compile, $rootScope, editor) {
+            var aceGutter = elm.find('.ace_gutter');
+
+            scope.sync = false;
+            scope.$apply();
+
+            expect(aceGutter).toHaveClass('inactive');
+
+            scope.sync = true;
+            scope.$apply();
+
+            expect(aceGutter).not.toHaveClass('inactive');
+        }));
     });
-  });
+
+    describe('star', function () {
+        beforeEach(angular.mock.inject(function ($rootScope, $compile) {
+            elm = angular.element('<star val="star"></star>');
+
+            scope = $rootScope;
+            scope.star = false;
+            $compile(elm)(scope);
+
+
+            scope.$digest();
+        }));
+
+        it('should change scope.val value when click', function () {
+            expect(scope.star).toBe(false);
+            elm.click();
+            expect(scope.star).toBe(true);
+            elm.click();
+            expect(scope.star).toBe(false);
+        });
+    });
+
+    describe('alert', function () {
+        beforeEach(angular.mock.inject(function ($rootScope, $compile, analytics) {
+            elm = angular.element('<alert></alert>');
+
+            scope = $rootScope;
+            scope.star = false;
+            $compile(elm)(scope);
+
+            scope.$digest();
+
+            analytics.pushAnalytics = jasmine.createSpy();
+        }));
+
+        it('should react to error event', angular.mock.inject(function (analytics) {
+            var data = {message: 'test'};
+            scope.$broadcast('error', data);
+
+            expect(analytics.pushAnalytics).toHaveBeenCalledWith('Error', data.message);
+            expect(scope.message).toEqual(data.message);
+            expect(elm).toHaveAttr('style','display: block;');
+        }));
+
+        it('should call close method on close element click', function () {
+            scope.close = jasmine.createSpy();
+            elm.find('.close').click();
+            expect(scope.close).toHaveBeenCalled();
+            expect(elm).toBeHidden();
+        })
+    });
+
+    describe('bootstrapSwitch', function () {
+        beforeEach(angular.mock.inject(function ($rootScope, $compile) {
+            elm = angular.element('<bootstrap-switch property="val"></bootstrap-switch>');
+
+            scope = $rootScope;
+            scope.val = null;
+            $compile(elm)(scope);
+
+            scope.$digest();
+        }));
+
+        it('should react to switch-change event', function () {
+            expect(scope.val).toEqual(null);
+            $(elm).trigger('switch-change', {value: true});
+            expect(scope.val).toEqual(true);
+        });
+
+        it('should watch property', function () {
+            scope.val = true;
+
+            scope.$apply();
+
+            expect($(elm).bootstrapSwitch('status')).toBe(true);
+
+            scope.val = false;
+            scope.$apply();
+
+            expect($(elm).bootstrapSwitch('status')).toBe(false);
+        });
+    })
 });
