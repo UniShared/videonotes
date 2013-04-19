fc6f4ec7fe6fa36200985081557d5cb9fda415e7
Arnaud BRETON <arnaud@unishared.com>
2013-04-19 14:48:39 -0700

    Analytics on document actions

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -361,7 +361,7 @@ controllersModule.controller('EditorCtrl', ['$scope', 'editor', 'doc', 'autosave
     $scope.init();
 }]);
 
-controllersModule.controller('ShareCtrl', ['$scope','config','doc', function($scope, config, doc) {
+controllersModule.controller('ShareCtrl', ['$scope','config','doc', 'analytics', function($scope, config, doc, analytics) {
     $scope.enabled = function () {
         return doc && doc.info && doc.info.id != null;
     };
@@ -369,6 +369,7 @@ controllersModule.controller('ShareCtrl', ['$scope','config','doc', function($sc
         var client = new gapi.drive.share.ShareClient(config.appId);
         client.setItemIds([doc.info.id]);
         client.showSettingsDialog();
+        analytics.pushAnalytics('Document', 'Share');
     }
 }]);
 
@@ -390,15 +391,15 @@ controllersModule.controller('MenuCtrl', ['$scope', '$rootScope', '$window', 'co
             .setCallback(angular.bind(this, onFilePicked))
             .build();
         picker.setVisible(true);
-        analytics.pushAnalytics('Open document');
+        analytics.pushAnalytics('Document', 'Open');
     };
     $scope.create = function () {
         editor.create();
-        analytics.pushAnalytics('Create new document');
+        analytics.pushAnalytics('Document', 'Create new');
     };
     $scope.save = function () {
         editor.save(true);
-        analytics.pushAnalytics('Save document');
+        analytics.pushAnalytics('Document', 'Save');
     };
 
     $scope.$watch('sync.enabled', function () {
@@ -442,7 +443,7 @@ controllersModule.controller('RenameCtrl', ['$scope', 'doc', 'analytics', functi
             $scope.$apply(function () {
                 $scope.newFileName = doc.info.title;
                 $scope.tour.next();
-                analytics.pushAnalytics('Rename document');
+                analytics.pushAnalytics('Document', 'Rename');
             });
         });
     $scope.save = function () {
