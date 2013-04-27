f2b618982a5953871bfed064811e65cbfa4ca696
Arnaud BRETON <arnaud@unishared.com>
2013-04-27 14:23:31 -0700

    Launching only Chrome

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
         karma: {
             options: {
                 configFile: 'config/karma.conf.js',
-                browsers: ['Chrome', 'Firefox']
+                browsers: ['Chrome']
             },
 
             unit: {
