8f62601fcd0b96d044fe3b6661f2554cef2bb92f
Arnaud BRETON <arnaud@unishared.com>
2013-04-27 19:29:35 -0700

    Angular UI select2

--- static/lib/angular-ui-custom/angular-ui-ieshiv.min.js
+++ static/lib/angular-ui-custom/angular-ui-ieshiv.min.js
@@ -1,6 +1,6 @@
 /**
  * AngularUI - The companion suite for AngularJS
- * @version v0.4.0 - 2013-04-07
+ * @version v0.4.0 - 2013-04-09
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
