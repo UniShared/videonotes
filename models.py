498fd6edb89ca66db959fd7526a71f9b65b40af3
Arnaud BRETON <arnaud@unishared.com>
2013-06-27 11:39:26 -0700

    Evernote credentials model

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, 'lib')
 
 from google.appengine.ext import db
-from google.appengine.ext.db import EmailProperty
+from google.appengine.ext.db import EmailProperty, StringProperty
 from oauth2client.appengine import CredentialsProperty
 
 __author__ = 'Arnaud BRETON (UniShared)'
@@ -22,6 +22,9 @@ class Credentials(db.Model):
     The CredentialsProperty is provided by the Google API Python Client, and is
     used by the Storage classes to store OAuth 2.0 credentials in the data store."""
     credentials = CredentialsProperty()
+
+class EvernoteCredentials(db.Model):
+    credentials = StringProperty()
 
 class RegisteredUser(db.Model):
     """
